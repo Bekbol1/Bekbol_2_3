eeb9c5c7f296658725c97c8d5b7e94a44051c01d
Bekbol1 <kbekbol9@gmail.com>
2023-05-15 16:22:26 +0600

    Terminal

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void deposit(double sum) {
+        amount += sum;
+    }
+
+    public void withDraw(double sum) throws LimitException {
+        if (sum > amount) {
+            throw new LimitException("Запрашиваемая больше чем сумма на балансе карты", amount);
+        }
+        amount -= sum;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws LimitException {
+        BankAccount  cardBalance = new BankAccount();
+        cardBalance.deposit(20000);
+        double withdrawalAmount = 6000;
+        while (true) {
+            try {
+                cardBalance.withDraw(withdrawalAmount);
+                System.out.println("Снято: " + withdrawalAmount + " сом");
+            } catch (LimitException e) {
+                System.out.println("Недостаточно для снятия такой суммы. Остаток на карте: " + e.getRemainingAmount());
+                withdrawalAmount = e.getRemainingAmount();
+                cardBalance.withDraw(withdrawalAmount);
+                System.out.println("Снято: " + withdrawalAmount + " сом");
+                break;
+            }
+        }
+
+        System.out.println("Баланс вашей карты: " + cardBalance.getAmount() +  " сом");
     }
 }
